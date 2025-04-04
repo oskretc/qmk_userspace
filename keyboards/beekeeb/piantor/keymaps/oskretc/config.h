0827d34119f59252c5cc4551c8eef4b3f23b87e4
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-04-04 23:00:42 +0200

    first commit

---
+++ keyboards/beekeeb/piantor/keymaps/oskretc/config.h
@@ -0,0 +1,42 @@
+/* Copyright 2023 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define RGB_MATRIX_SLEEP
+#define RGB_MATRIX_KEYPRESSES
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#define PIMORONI_TRACKBALL_ADDRESS 0x0A
+#define PIMORONI_TRACKBALL_ROTATE
+#define SPLIT_POINTING_ENABLE
+// #define POINTING_DEVICE_ROTATION_180_RIGHT
+#define POINTING_DEVICE_RIGHT
+// #define LED_CAPS_LOCK_PIN 24
+// #define LED_PIN_ON_STATE 0
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 280
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_INTERVAL 8
+#define MOUSEKEY_MOVE_DELTA 12
+#define MOUSEKEY_INITIAL_SPEED 64
+#define COMBO_ONLY_FROM_LAYER 0
